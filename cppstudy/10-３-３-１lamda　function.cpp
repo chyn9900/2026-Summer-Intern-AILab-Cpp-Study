1e3314f3400ea8a30bb3d8997a8de0fa8505190d
chyn9900 <chyn8800@gmail.com>
2026-07-16 15:42:49 +0900

    깃커밋

---
+++ cppstudy/10-３-３-１lamda　function.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <string>
+
+template <typename Iter>
+void print(Iter begin, Iter end) {
+	while (begin != end) {
+		std::cout << "[" << *begin << "] ";
+		begin++;
+	}
+	std::cout << std::endl;
+}
+int main() {
+	std::vector<int> vec;
+	vec.push_back(5);
+	vec.push_back(3);
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+
+	std::cout << "처음 vec 상태 ------" << std::endl;
+	print(vec.begin(), vec.end());
+
+	std::cout << "벡터에서 홀수인 원소 제거 ---" << std::endl;
+	vec.erase(std::remove_if(vec.begin(), vec.end(),
+						[](int i)-> bool {return i % 2 == 1; }),
+		  vec.end());
+	print(vec.begin(), vec.end());
+
+}
